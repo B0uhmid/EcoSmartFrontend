94106fbf05ab7e778238be337ddbce10aa91a65e
Hammami Ahmed <hammamiahmed763@gmail.com>
2026-04-23 21:20:55 +0100

    Initial commit React app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import './components/navbar.jsx'
+import Navbar from "./components/navbar.jsx";
+
+function App() {
+      return(
+          <Navbar/>
+      );
+}
+
+export default App
