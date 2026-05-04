2803567ca631e9ac6da1cd943be3138f5260620d
ismail01-jl <jellitiismail2002@gmail.com>
2026-05-04 23:37:16 +0100

    Adding GET endpoints

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,27 @@
 const API_URL = import.meta.env.VITE_BACK_URL
 
+// GET endpoints :
+
+export const getCategories = async () => {
+    const res = await fetch(`${BASE_URL}/categories`)
+    return res.json()
+}
+
+export const getSources = async () => {
+    const res = await fetch(`${BASE_URL}/sources`)
+    return res.json()
+}
+
+export const getStats = async () => {
+    const res = await fetch(`${BASE_URL}/stats`)
+    return res.json()
+}
+
+export const getStatsByCategory = async (categorie) => {
+    const res = await fetch(`${BASE_URL}/stats/${categorie}`)
+    return res.json()
+}
+
 export const predictFull = async (lotData) => {
     const res = await fetch(`${API_URL}/predict/full`, {
         method: "POST",
