e4261f8e75951efd691fdd8804ab4a55a82c0c41
ismail01-jl <jellitiismail2002@gmail.com>
2026-05-06 21:07:40 +0100

    Fix GET endpoints

--- src/services/api.js
+++ src/services/api.js
@@ -3,25 +3,25 @@ const API_URL = import.meta.env.VITE_BACK_URL
 // GET endpoints :
 
 export const getCategories = async () => {
-    const res = await fetch(`${BASE_URL}/categories`)
+    const res = await fetch(`${API_URL}/categories`)
     return res.json()
 }
 
 export const getSources = async () => {
-    const res = await fetch(`${BASE_URL}/sources`)
+    const res = await fetch(`${API_URL}/sources`)
     return res.json()
 }
 
 export const getStats = async () => {
-    const res = await fetch(`${BASE_URL}/stats`)
+    const res = await fetch(`${API_URL}/stats`)
     return res.json()
 }
 
 export const getStatsByCategory = async (categorie) => {
-    const res = await fetch(`${BASE_URL}/stats/${categorie}`)
+    const res = await fetch(`${API_URL}/stats/${categorie}`)
     return res.json()
 }
-
+// POST endpoints :
 export const predictFull = async (lotData) => {
     const res = await fetch(`${API_URL}/predict/full`, {
         method: "POST",
